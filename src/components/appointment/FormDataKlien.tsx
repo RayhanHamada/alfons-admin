969f785a05151dbad453800356e396e35053eebb
RayhanHamada <rayhanhamada1999@gmail.com>
2022-01-11 13:37:23 +0700

    feat: add klien detail when selected

--- src/components/appointment/FormDataKlien.tsx
+++ src/components/appointment/FormDataKlien.tsx
@@ -4,7 +4,9 @@ import {
   Form,
   Row,
   Select,
+  Spin,
   Typography,
+  useOne,
   useSelect,
 } from '@pankod/refine';
 import type { CreateAppointmentUserDataFormValue } from '@utility/hooks/useCreateAppointmentStore';
@@ -14,7 +16,20 @@ import { MouseEventHandler } from 'react';
 const { Title, Text } = Typography;
 
 const FormDataKlien: React.FC = (_props) => {
-  const { toggleCreateKlienDrawer, setKlienId } = useCreateAppointmentStore();
+  const { toggleCreateKlienDrawer, setKlienId, klienId } =
+    useCreateAppointmentStore();
+
+  const {
+    data: klienData,
+    isError: isKlienError,
+    isLoading: isKlienLoading,
+  } = useOne<IKlien>({
+    resource: 'klien',
+    id: klienId,
+    queryOptions: {
+      enabled: klienId !== '',
+    },
+  });
 
   const { selectProps: selectKlienProps } = useSelect<IKlien>({
     resource: 'klien',
@@ -63,6 +78,22 @@ const FormDataKlien: React.FC = (_props) => {
           }}
         />
       </Form.Item>
+
+      {isKlienError ? (
+        <Text>Gagal mengambil data klien</Text>
+      ) : isKlienLoading ? (
+        <Spin spinning />
+      ) : klienData ? (
+        <>
+          <Title level={5}>Nama</Title>
+          {klienData?.data.jenis_kelamin === 'PRIA' ? 'Tn. ' : 'Ny. '}
+          {klienData?.data.name}
+          <Title level={5}>Nomor Telepon</Title>
+          {klienData?.data.phone_number}
+          <Title level={5}>Jenis Kelamin</Title>
+          {klienData?.data.jenis_kelamin.toLowerCase()}
+        </>
+      ) : undefined}
     </Form>
   );
 };

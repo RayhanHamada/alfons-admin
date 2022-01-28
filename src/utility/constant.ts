fc90bc6e129ed4c34c442abf018eeb6668b4617f
RayhanHamada <rayhanhamada1999@gmail.com>
2022-01-28 14:01:37 +0700

    feat: change base api url

--- src/utility/constant.ts
+++ src/utility/constant.ts
@@ -2,4 +2,4 @@ export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 export const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 export const supabaseApiURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const supabaseAnonRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-export const baseAPIURL = `${baseURL}/api`;
+export const baseAPIURL = `/api`;

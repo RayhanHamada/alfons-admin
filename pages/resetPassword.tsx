0edd6c266376a009edf845d42f3afd8fc9880111
RayhanHamada <rayhanhamada1999@gmail.com>
2022-02-02 00:05:12 +0700

    fix: resetPassword

--- pages/resetPassword.tsx
+++ pages/resetPassword.tsx
@@ -1,46 +1,11 @@
 import { ResetPasswordForm } from '@components/ResetPasswordForm';
-import { AntdLayout, Col, Typography } from '@pankod/refine';
+import { AntdLayout } from '@pankod/refine';
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-
-const { Title, Text } = Typography;
 
 const ResetPassword: NextPage = (_props) => {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (window.location.hash.includes('type=recovery')) {
-      const hash = window.location.hash.split('#')[1];
-      const query = new URLSearchParams(hash);
-      setAccessToken(query.get('access_token'));
-    }
-  }, []);
-
   return (
     <AntdLayout style={{ height: '100vh', width: '100%' }}>
-      <Col
-        style={{
-          textAlign: 'center',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        {accessToken ? (
-          <>
-            <Title level={3}>Reset Password anda</Title>
-            <Text>Masukkan password baru anda</Text>
-            <ResetPasswordForm accessToken={accessToken} />
-          </>
-        ) : (
-          <>
-            <Title level={4}>
-              Link Reset Password sudah kadaluarsa. Silahkan buat permintaan
-              untuk reset password lagi.
-            </Title>
-          </>
-        )}
-      </Col>
+      <ResetPasswordForm />
     </AntdLayout>
   );
 };
--- src/components/ResetPasswordForm.tsx
+++ src/components/ResetPasswordForm.tsx
@@ -1,11 +1,21 @@
-import { Button, Form, Icons, Input, message, Spin } from '@pankod/refine';
+import {
+  Button,
+  Col,
+  Form,
+  Icons,
+  Input,
+  message,
+  Spin,
+  Typography,
+} from '@pankod/refine';
 import { updatePassword } from '@utility/api';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const { Password } = Input;
 const { LoadingOutlined } = Icons;
+const { Title, Text } = Typography;
 
 type IForm = {
   password?: string;
@@ -13,15 +23,23 @@ type IForm = {
 
 type OnFinish = (v: IForm) => void;
 
-type Props = {
-  accessToken: string;
-};
-
-export const ResetPasswordForm: React.FC<Props> = ({ accessToken }) => {
+export const ResetPasswordForm: React.FC = (_props) => {
   const router = useRouter();
   const [form] = Form.useForm<IForm>();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [accessToken, setAccessToken] = useState<string>();
+
+  useEffect(() => {
+    if (window.location.hash.includes('type=recovery')) {
+      const hash = window.location.hash.split('#')[1];
+      const query = new URLSearchParams(hash);
+      const at = query.get('access_token');
+
+      if (at) setAccessToken(at);
+    }
+  }, []);
 
   const onFinish: OnFinish = async ({ password }) => {
     if (!password || password === '') {
@@ -30,7 +48,7 @@ export const ResetPasswordForm: React.FC<Props> = ({ accessToken }) => {
     }
 
     setIsSubmitting(true);
-    const res = await updatePassword(accessToken, password);
+    const res = await updatePassword(accessToken!, password);
 
     if (!res.ok)
       return message.error(
@@ -49,35 +67,62 @@ export const ResetPasswordForm: React.FC<Props> = ({ accessToken }) => {
   };
 
   return (
-    <Form
-      layout="vertical"
-      style={{ width: '400px', marginTop: '30px' }}
-      form={form}
-      onFinish={onFinish}
-      requiredMark
-    >
-      <Form.Item
-        name="password"
-        rules={[{ required: true, min: 8 }]}
-        style={{ marginBottom: '12px' }}
+    <>
+      <Col
+        style={{
+          textAlign: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
       >
-        <Password placeholder="Password baru anda" minLength={8} />
-      </Form.Item>
+        {accessToken ? (
+          <>
+            <Title level={3}>Reset Password anda</Title>
+            <Text>Masukkan password baru anda</Text>
+            <Form
+              layout="vertical"
+              style={{ width: '400px', marginTop: '30px' }}
+              form={form}
+              onFinish={onFinish}
+              requiredMark
+            >
+              <Form.Item
+                name="password"
+                rules={[{ required: true, min: 8 }]}
+                style={{ marginBottom: '12px' }}
+              >
+                <Password placeholder="Password baru anda" minLength={8} />
+              </Form.Item>
 
-      <Spin indicator={<LoadingOutlined spin />} spinning={isSubmitting}>
-        <Button
-          type="primary"
-          size="large"
-          htmlType="submit"
-          disabled={isSubmitting}
-        >
-          Reset Password !
-        </Button>
-      </Spin>
-      <br />
-      <u hidden={isSubmitting}>
-        <Link href="/login">Batalkan dan ke login</Link>
-      </u>
-    </Form>
+              <Spin
+                indicator={<LoadingOutlined spin />}
+                spinning={isSubmitting}
+              >
+                <Button
+                  type="primary"
+                  size="large"
+                  htmlType="submit"
+                  disabled={isSubmitting}
+                >
+                  Reset Password !
+                </Button>
+              </Spin>
+              <br />
+              <u hidden={isSubmitting}>
+                <Link href="/login">Batalkan dan ke login</Link>
+              </u>
+            </Form>
+          </>
+        ) : (
+          <>
+            <Title level={4}>
+              Link Reset Password sudah kadaluarsa. Silahkan buat permintaan
+              untuk reset password lagi.
+            </Title>
+          </>
+        )}
+      </Col>
+    </>
   );
 };

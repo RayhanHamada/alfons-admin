f11252fe5de695b50c8dfd5298d54e47b93be91f
RayhanHamada <rayhanhamada1999@gmail.com>
2022-02-01 23:15:47 +0700

    fix: invalid dataProvider type

--- src/utility/dataProvider.ts
+++ src/utility/dataProvider.ts
@@ -1,4 +1,7 @@
 import { dataProvider as supabaseDataProvider } from '@pankod/refine-supabase';
+import { SupabaseClient } from '@supabase/supabase-js';
 import { supabaseBrowserClient } from '@utility/supabaseBrowserClient';
 
-export const dataProvider = supabaseDataProvider(supabaseBrowserClient);
+export const dataProvider = supabaseDataProvider(
+  supabaseBrowserClient as unknown as SupabaseClient
+);
